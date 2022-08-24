ec38e2391207ce34f9e304bbcf75b0de1fce793e
Andres Loor <andresloor@gmail.com>
2022-08-24 05:44:48 -0500

    Some tests of API GET

--- django_api/consume.py
+++ django_api/consume.py
@@ -3,3 +3,9 @@ import requests
 
 response = requests.get('http://127.0.0.1:8000/rooms/')
 print(response.json())
+
+response = requests.get('http://127.0.0.1:8000/events/')
+print(response.json())
+
+response = requests.get('http://127.0.0.1:8000/booking/')
+print(response.json())
